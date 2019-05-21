d7ae2182208a10872c9ce94533a8648654cdc970
Marcus Baitz <marcus.baitz@bakdata.com>
2019-05-21 09:59:55 +0200

    formatting and default value for next_task

--- dags/util/gc_tasks.py
+++ dags/util/gc_tasks.py
@@ -1,20 +1,23 @@
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 from airflow.contrib.operators.gcs_to_gcs import GoogleCloudStorageToGoogleCloudStorageOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 
 def _create_merge_sql(source, target, schema, **context):
     columns = [item["name"] for item in schema]
     columns = ",".join(columns)
-    return f"""MERGE `{target}` T
-            USING `{source}` S
-            ON T.cdc_hash = TO_BASE64(MD5(TO_JSON_STRING(S)))
-            WHEN NOT MATCHED THEN
-              INSERT ({columns}, inserted_at, cdc_hash)
-              VALUES ({columns}, CURRENT_DATETIME(), TO_BASE64(MD5(TO_JSON_STRING(S))))"""
+    return f"""
+        MERGE `{target}` T
+        USING `{source}` S
+        ON T.cdc_hash = TO_BASE64(MD5(TO_JSON_STRING(S)))
+        WHEN NOT MATCHED THEN
+          INSERT ({columns}, inserted_at, cdc_hash)
+          VALUES ({columns}, CURRENT_DATETIME(), TO_BASE64(MD5(TO_JSON_STRING(S))))
+    """
 
 
-def gc_tasks(name, schema, next_task):
+def gc_tasks(name, schema, next_task=DummyOperator(task_id="Done")):
     bq_staging = "{{{{ var.value.gc_project_id }}}}.{{{{ var.value.bq_dataset_source }}}}.{0}".format(name)
     bq_warehouse = "{{{{ var.value.gc_project_id }}}}.{{{{ var.value.bq_dataset_target }}}}.{0}".format(name)
 
