80e67419f29a50c80cb7ac01574bfa2c4acefab4
Frederic Schneider <frederic.schneider@student.hpi.uni-potsdam.de>
2019-06-04 15:23:58 +0200

    prompt for proj_id in data upload script

--- scripts/google_upload_data.sh
+++ scripts/google_upload_data.sh
@@ -2,6 +2,8 @@
 
 pushd $( dirname "${BASH_SOURCE[0]}" ) >/dev/null 2>&1
 
+read -p "please enter your project id: " GC_PROJECT_ID
+
 gsutil -m cp ../data/matchweek/* gs://${GC_PROJECT_ID} && \
 gsutil -m cp ../data/scorer/* gs://${GC_PROJECT_ID}
 
